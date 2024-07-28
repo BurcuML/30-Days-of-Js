310233996f0bd43de8a10d4a2a98f1c00d751d8c
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2024-07-28 03:30:34 +0300

    updated

--- Gradient Generator/main.js
+++ Gradient Generator/main.js
@@ -2,19 +2,18 @@ const color1 = document.getElementById("color1")
 const color2 = document.getElementById("color2")
 
 
-
 function generateLinear() {
     const gradientString = `linear-gradient(to right, ${color1.value}, ${color2.value})`
     document.body.style.background = gradientString;
     console.log(gradientString)
 }
 function generateRadial() {
-    const gradientString = `linear-gradient(to right, ${color1.value}, ${color2.value})`
+    const gradientString = `radial-gradient(${color1.value}, ${color2.value})`
     document.body.style.background = gradientString;
     console.log(gradientString)
 }
 function generateConic() {
-    const gradientString = `linear-gradient(to right, ${color1.value}, ${color2.value})`
+    const gradientString = `conic-gradient(${color1.value}, ${color2.value})`
     document.body.style.background = gradientString;
     console.log(gradientString)
 }
