5ef88c5fe261018e217406789492ec7aeb8e8df0
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2024-08-06 00:55:13 +0300

    updated

--- ToDo List/main.js
+++ ToDo List/main.js
@@ -12,6 +12,9 @@ function addTodo(){
     let li = document.createElement("li");
     ul.appendChild(li);
     li.textContent= text;
+    li.style.marginBottom= "15px"
+    li.style.width="250px"
+    li.style.wordBreak="break-all"
     document.querySelector("textarea").value = "";
     }
 
