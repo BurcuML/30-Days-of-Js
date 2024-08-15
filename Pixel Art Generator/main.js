c7a258a26a53cadda446d3ad0f3c1e635e5d8747
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2024-08-16 00:04:16 +0300

    updated

--- Pixel Art Generator/main.js
+++ Pixel Art Generator/main.js
@@ -1,2 +1,16 @@
 const createGrid = document.getElementById("createGrid");
 const clearGrid = document.getElementById("clearGrid")
+const width = document.getElementById("width")
+const height = document.getElementById("height")
+const pwidth = document.getElementById("pwidth")
+const pheight = document.getElementById("pheight")
+
+function updateWidth(val) {
+    width.value=val; 
+    pwidth.innerText= val
+  }
+  
+function updateHeight(val2) {
+    height.value=val2; 
+    pheight.innerText= val2
+  }
